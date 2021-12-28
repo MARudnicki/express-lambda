364cb10069f83c73f99d1207066e7160d6f20c84
Maciej Rudnicki <maciejru@amazon.com>
2021-12-28 09:47:07 +0100

    fix endpoint variable

--- test/integ.test.js
+++ test/integ.test.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 test("Service returns 'Hello World!'", async () => {
   try {
     console.log(`maciejru Variable is ${process.env.SERVICE_ENDPOINT}`)
-    const response = await axios.get(process.env.dupa, {});
+    const response = await axios.get(process.env.SERVICE_ENDPOINT, {});
     expect(response.data).toBe("Hello World!");
   } catch (e) {
     console.error(e);
